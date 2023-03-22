6573ed4bd2ce816fd5177f7872ceb49bddfc8c82
GiorgiShushanashvili <126264628+GiorgiShushanashvili@users.noreply.github.com>
2023-03-22 05:20:42 +0400

    second commit (#1)

--- New-Homework/Microfinance.cs
+++ New-Homework/Microfinance.cs
@@ -3,7 +3,33 @@ namespace New_Homework
 {
 	public class Microfinance:IFinanceoperations
 	{
-		
-	}
+        public bool ischecked = true;
+        private double percent = 0.1;
+        private double commision = 4;
+        public double loan { get; set; }
+        public int m { get; set; }
+        public double sum { get; set; }
+        public double wholesum { get; set; }
+
+
+        public void CheckUserHstory()
+        {
+            Console.WriteLine(ischecked);
+        }
+
+
+        public void CalculateLoanPercnt(int month, double amountpermonth)
+        {
+            month = m;
+            amountpermonth = loan / m;
+
+            sum = (loan * percent + loan) / m + commision;
+            Console.WriteLine($"the sum you have to pay each month is {sum}");
+            wholesum = sum * month;
+            Console.WriteLine($"whole sum you should pay during loan period is {wholesum}");
+
+
+        }
+    }
 }
 
