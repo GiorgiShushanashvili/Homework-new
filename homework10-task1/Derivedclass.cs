05bc276c191760043a5532aa6a8009649ef36c26
GiorgiShushanashvili <126264628+GiorgiShushanashvili@users.noreply.github.com>
2023-03-22 05:47:11 +0400

    new changes (#2)

---
+++ homework10-task1/Derivedclass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace homework10_task1
+{
+	public class Derivedclass:Fileworker
+	{
+        
+        
+            private string fileexpend = "";
+
+            public override string fileexpenditure
+            {
+                get => fileexpend;
+                set => fileexpend = value;
+            }
+
+            public override void Delete()
+            {
+
+            }
+            public override void Edit()
+            {
+
+            }
+
+            public override void Read()
+            {
+
+            }
+
+            public override void Write()
+            {
+
+            }
+        
+    }
+}
+
---
+++ homework10-task1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace homework10_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
