7e53edc165ad34c4c155a5a8e33f24a3e8a5f632
GiorgiShushanashvili <shushanashvili20@gmail.com>
2023-03-25 08:23:51 +0400

    change5

---
+++ Homework11-Patterns/Furniture.cs
@@ -0,0 +1,80 @@
+﻿using System;
+namespace Homework11_Patterns
+{
+    public interface Ifurniture
+    {
+        void Finalchoice();
+    }
+
+    public class VictorianChair : Ifurniture
+    {
+        public void Finalchoice()
+        {
+            Console.WriteLine($"chosen product is {this}");
+        }
+    }
+    public class Modernchair : Ifurniture
+    {
+        public void Finalchoice()
+        {
+            Console.WriteLine($"chosen product is {this}");
+        }
+    }
+    public class Artdecochair : Ifurniture
+    {
+        public void Finalchoice()
+        {
+            Console.WriteLine($"chosen product is {this}");
+        }
+    }
+
+
+    public class VictorianCoffetable : Ifurniture
+    {
+        public void Finalchoice()
+        {
+            Console.WriteLine($"chosen product is {this}");
+        }
+    }
+    public class Moderncoffetable : Ifurniture
+    {
+        public void Finalchoice()
+        {
+            Console.WriteLine($"chosen product is {this}");
+        }
+    }
+    public class Artdecoctable : Ifurniture
+    {
+        public void Finalchoice()
+        {
+            Console.WriteLine($"chosen product is {this}");
+        }
+    }
+
+
+
+    public class Victoriansofa : Ifurniture
+    {
+        public void Finalchoice()
+        {
+            Console.WriteLine($"chosen product is {this}");
+        }
+    }
+
+    public class Modernsofa : Ifurniture
+    {
+        public void Finalchoice()
+        {
+            Console.WriteLine($"chosen product is {this}");
+        }
+    }
+
+    public class ArtdecoSofa : Ifurniture
+    {
+        public void Finalchoice()
+        {
+            Console.WriteLine($"chosen product is {this}");
+        }
+    }
+}
+
