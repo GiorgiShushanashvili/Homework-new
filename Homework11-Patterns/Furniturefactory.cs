2f06cc3f5aabf533ff23bdc1fdd1a62888081b45
GiorgiShushanashvili <126264628+GiorgiShushanashvili@users.noreply.github.com>
2023-03-25 08:25:30 +0400

    change5 (#3)

---
+++ Homework11-Patterns/Furniturefactory.cs
@@ -0,0 +1,70 @@
+﻿using System;
+namespace Homework11_Patterns
+{
+    public interface IFurnitureFactory
+    {
+        public Ifurniture createchair();
+        public Ifurniture createcoffeetable();
+        public Ifurniture createsofa();
+
+    }
+
+    public class VictorianFactory : IFurnitureFactory
+    {
+        public Ifurniture createchair()
+        {
+            return new VictorianChair();
+        }
+
+        public Ifurniture createcoffeetable()
+        {
+            return new VictorianCoffetable();
+        }
+
+        public Ifurniture createsofa()
+        {
+            return new Victoriansofa();
+        }
+    }
+
+
+
+    public class Modernfactory : IFurnitureFactory
+    {
+        public Ifurniture createchair()
+        {
+            return new Modernchair();
+        }
+
+        public Ifurniture createcoffeetable()
+        {
+            return new Moderncoffetable();
+        }
+
+        public Ifurniture createsofa()
+        {
+            return new Modernsofa();
+        }
+    }
+
+
+
+    public class Artdecofactory : IFurnitureFactory
+    {
+        public Ifurniture createchair()
+        {
+            return new Artdecochair();
+        }
+
+        public Ifurniture createcoffeetable()
+        {
+            return new Artdecoctable();
+        }
+
+        public Ifurniture createsofa()
+        {
+            return new ArtdecoSofa();
+        }
+    }
+}
+
---
+++ Homework11-Patterns/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Homework11_Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int style;
+            do {
+                Console.WriteLine("select your desired style");
+                Console.WriteLine("press 1,2 or 3, 1 - for victorian, 2 - modern, 3 - artdeco");
+                int.TryParse(Console.ReadLine(), out style); }
+            while (style ==0||style>3);
+            IFurnitureFactory factory = null;
+            switch (style)
+            {
+                case 1:
+                    factory =new VictorianFactory();
+                    break;
+                case 2:
+                    factory = new Modernfactory();
+                    break;
+                case 3:
+                    factory = new Artdecofactory();
+                    break;
+            }
+
+            int product;
+            do
+            {
+                Console.WriteLine("select your desired furniture");
+                Console.WriteLine("press 1 - chair, 2 - table, 3 - for sofa");
+                int.TryParse(Console.ReadLine(), out product);
+            }
+            while (product==0||product>3);
+            Ifurniture furniture = null;
+            switch (product)
+            {
+                case 1:
+                    furniture = factory.createchair();
+                    break;
+                case 2:
+                    furniture = factory.createcoffeetable();
+                    break;
+                case 3:
+                    furniture = factory.createsofa();
+                    break;
+            }
+            Console.WriteLine("furniture created");
+            furniture.Finalchoice();
+
+            
+
+
+
+            Console.ReadLine();  
+
+        }
+    }
+
+    
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
